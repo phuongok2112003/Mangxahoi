74f1d05f9c6d3532a1f17aeba47d5b95428ca0a6
Nguyen Xuan Phuong <nguyenxuanphuong@luvina.net>
2024-10-15 15:13:22 +0700

    first commit

---
+++ src/main/java/com/example/Mangxahoi/MangxahoiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Mangxahoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangxahoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangxahoiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Mangxahoi/MangxahoiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Mangxahoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangxahoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
