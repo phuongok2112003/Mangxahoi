3f95df7e08cf6583a343caf10b25bc4447b98788
phuongok2112003 <phuong0961070156@gmail.com>
2024-11-26 15:44:34 +0700

    version 2.2.

--- src/main/java/com/example/Mangxahoi/error/CommonStatus.java
+++ src/main/java/com/example/Mangxahoi/error/CommonStatus.java
@@ -6,9 +6,9 @@ import lombok.AllArgsConstructor;
 public enum CommonStatus implements ErrorStatus {
     SUCCESS(200, "messages.success"),
     FORBIDDEN(403, "errors.access_denied"),
-    TokenIsInvalid(401,"Invalid token"),
-    TokenExpired(401,"errors.The Token has expired"),
-    OtpExpired(401,"errors.The Otp has expired"),
+    TokenIsInvalid(401,"errors.invalid token"),
+    TokenExpired(401,"errors.the Token has expired"),
+    OtpExpired(401,"errors.the Otp has expired"),
     ACCOUNT_NOT_FOUND(400, "errors.account_not_found"),
     ACCOUNT_LOG_TOO(400, "errors.Your account has logged in too many times"),
     PASSWORD_NOT_CONFIRM(400,"errors.password_not_confirm"),
