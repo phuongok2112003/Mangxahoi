5dad024e4cf1438ebc31d60954f99c3580a26fef
phuongok2112003 <phuong0961070156@gmail.com>
2024-12-03 15:50:27 +0700

    update 4.0.2

--- src/main/java/com/example/Mangxahoi/error/DataError.java
+++ src/main/java/com/example/Mangxahoi/error/DataError.java
@@ -15,32 +15,32 @@ public class DataError<T> {
     public DataError() {
     }
 
-    public static <T> DataError<T> build(ErrorStatus status){
-        DataError<T> response = new DataError<>();
+    public static  DataError<ApiMessageError> build(ErrorStatus status){
+        DataError<ApiMessageError> response = new DataError<>();
         response.message =CommonStatus.FAILURE.getMessage();
-        response.data= (T) new ApiMessageError( EbsI18n.get(status.getMessage()));
+        response.data=  new ApiMessageError( EbsI18n.get(status.getMessage()));
         return response;
     }
 
-    public static <T> DataError<T> build(){
-        return build(CommonStatus.SUCCESS);
-    }
+//    public static <T> DataError<T> build(){
+//        return build(CommonStatus.SUCCESS);
+//    }
 
-    public static <T> DataError<T> build(int code, String message) {
-        DataError<T> response = new DataError<>();
-        response.message = EbsI18n.get(message);
-        return response;
-    }
+//    public static <T> DataError<T> build(int code, String message) {
+//        DataError<T> response = new DataError<>();
+//        response.message = EbsI18n.get(message);
+//        return response;
+//    }
+//
+//    public static <T> DataError<T> build(T data, ErrorStatus status) {
+//        DataError<T> response = build(status);
+//        response.data = data;
+//        return response;
+//    }
 
-    public static <T> DataError<T> build(T data, ErrorStatus status) {
-        DataError<T> response = build(status);
-        response.data = data;
-        return response;
-    }
-
-    public static <T> DataError<T> build(T data) {
-        DataError<T> response = build();
-        response.data = data;
-        return response;
-    }
+//    public static <T> DataError<T> build(T data) {
+//        DataError<T> response = build();
+//        response.data = data;
+//        return response;
+//    }
 }
